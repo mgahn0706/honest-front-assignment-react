9322ee6aa1bdbfba64998183076c510034589b66
안민규 <mgahn0706@snu.ac.kr>
2022-06-11 18:39:28 +0900

    본인인증 화면 기본 틀 구현
    디자인 적용 안한 화면

--- src/IdentityAuthentication.js
+++ src/IdentityAuthentication.js
@@ -1,5 +1,42 @@
+import {useState} from "react";
+
 function IdentityAuthentication() {
-  return <main>본인인증 화면</main>;
+
+  const [phoneNum, setPhoneNum ] = useState(["010","",""])
+  const [civilCode, setCivilCode] = useState(["",""]);
+  const [userName, setUserName] = useState("");
+
+
+  return <div>
+    <h1>비대면 대출을 위해 본인인증이 필요해요</h1>
+    <div className="phone">
+        <label>휴대폰번호</label>
+      <div className="phoneInput">
+        <input maxLength={3}/>
+        <input maxLength={4}/>
+        <input maxLength={4}/>
+      </div>
+    </div>
+
+    <div className="civil">
+        <label>주민등록번호</label>
+      <div className="civilInput">
+        <input placeholder="앞 6자리" maxLength={6}/>
+        <input placeholder="뒤 7자리" type={"password"} maxLength={7}/>
+      </div>
+    </div>
+
+    <div className="name">
+      <label>주민등록번호</label>
+      <div className="nameInput">
+        <input placeholder="이름을 이벽해 주세요"/>
+      </div>
+    </div>
+
+
+    <button className="nextButton">다음</button>
+
+  </div>
 }
 
 export default IdentityAuthentication;
