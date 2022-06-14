dd739f19726a02e2c8d9c6949998281a429d6eda
안민규 <mgahn0706@snu.ac.kr>
2022-06-14 11:26:54 +0900

    feat: 휴대전화 인증 화면 구현
    인증 결과에 따른 처리는 미구현

--- src/PhoneCertification.js
+++ src/PhoneCertification.js
@@ -1,5 +1,47 @@
+import {useEffect, useState} from "react";
+import {postPhoneCode, postUserInfo} from "./API/API";
+
 function PhoneCertification() {
-  return <main>휴대전화 본인인증 코드 입력 화면</main>;
+
+    const [code, setCode] = useState("");
+    const [token, setToken] = useState("");
+
+    useEffect(()=>{if(!token){
+        setToken(localStorage.getItem("token"));
+    }})
+
+    const confirmCode = () => {
+
+        const codeInfo = {
+            code: code,
+            token: token,
+        }
+        console.log(codeInfo)
+        postPhoneCode(codeInfo).then((res)=>{
+            window.alert("성공")
+        })
+            .catch(()=>{
+                window.alert("실패");
+            })
+
+    }
+
+
+  return (
+      <div className="container">
+        <h1>휴대폰 번호로 전송된 인증번호를 입력해 주세요</h1>
+        <label>인증번호</label>
+          <div className="inputWrapper">
+              <input
+                  placeholder="번호 6자리를 입력해주세요"
+                  maxLength={6}
+                  onChange={(e)=>{setCode(e.target.value)}}/>
+          </div>
+          {code.length===6 ? <button className="confirmButton" onClick={()=>{confirmCode()}}>본인인증하기</button> :
+              <button className="confirmButtonDisable" disabled>본인인증하기</button>}
+
+      </div>
+  )
 }
 
 export default PhoneCertification;
